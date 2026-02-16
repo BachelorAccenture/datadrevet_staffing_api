2083b3437957e5d7fd0f806d4a9305aff9d21088
hugoolov <hugo@harnaes.no>
2026-02-16 12:40:39 +0100

    prøver å fikse tester

--- src/test/java/com/example/demo/controller/CompanyControllerTest.java
+++ src/test/java/com/example/demo/controller/CompanyControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.config.Neo4jTestContainerConfig;
+import com.example.demo.config.TestSecurityConfig;
 import com.example.demo.dto.request.CreateCompanyRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,8 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.core.Neo4jClient;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class CompanyControllerTest extends Neo4jTestContainerConfig {
 
     @Autowired
@@ -34,6 +39,8 @@ class CompanyControllerTest extends Neo4jTestContainerConfig {
 
     @Autowired
     private Neo4jClient neo4jClient;
+
+
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
